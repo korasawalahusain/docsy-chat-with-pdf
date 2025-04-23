418c85eac44d22c3581ae903f215a78f7a1cf229
Husain Korasawala <korasawala.husain@gmail.com>
2025-04-23 20:14:07 +0530

    [updated] updated env name live url

--- lib/getBaseUrl.ts
+++ lib/getBaseUrl.ts
@@ -1,6 +1,6 @@
 const getBaseUrl = () =>
   process.env.NODE_ENV === "development"
     ? "http://localhost:3000"
-    : `https://${process.env.VERCEL_URL}`;
+    : `https://${process.env.LIVE_URL}`;
 
 export default getBaseUrl;
