b16016f0d77ac09e11541f0586ed2177326eeb0f
Husain Korasawala <korasawala.husain@gmail.com>
2025-04-24 16:42:12 +0530

    [bugfix] updated firebase config to env .

--- firebase.ts
+++ firebase.ts
@@ -3,14 +3,7 @@ import { getFirestore } from "firebase/firestore";
 import { getApp, getApps, initializeApp } from "firebase/app";
 
 // Your web app's Firebase configuration
-const firebaseConfig = {
-  apiKey: "AIzaSyCyAfoarPgdcNda8jQ7snB3jiuYh9nNymM",
-  authDomain: "pdf-to-ai-chat.firebaseapp.com",
-  projectId: "pdf-to-ai-chat",
-  storageBucket: "pdf-to-ai-chat.firebasestorage.app",
-  messagingSenderId: "1053665058456",
-  appId: "1:1053665058456:web:574d104a3ef88a686bfaed",
-};
+const firebaseConfig = JSON.parse(process.env.FIREBASE_CONFIG as string);
 
 const app = getApps().length === 0 ? initializeApp(firebaseConfig) : getApp();
 
