d0adb072c150b3b74b5afae2de3d4cbf6fe5e746
Husain Korasawala <korasawala.husain@gmail.com>
2025-04-24 17:22:18 +0530

    [bugfix] added NEXT_PUBLIC_ to firebase config env var

--- firebase.ts
+++ firebase.ts
@@ -3,7 +3,9 @@ import { getFirestore } from "firebase/firestore";
 import { getApp, getApps, initializeApp } from "firebase/app";
 
 // Your web app's Firebase configuration
-const firebaseConfig = JSON.parse(process.env.FIREBASE_CONFIG as string);
+const firebaseConfig = JSON.parse(
+  process.env.NEXT_PUBLIC_FIREBASE_CONFIG as string
+);
 
 const app = getApps().length === 0 ? initializeApp(firebaseConfig) : getApp();
 
